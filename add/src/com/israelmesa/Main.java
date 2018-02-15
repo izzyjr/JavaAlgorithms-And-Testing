ec79c0a8bf5a90d4022b0e643dddf772f3ef7a7b
Israel Mesa <mesa.israel50@gmail.com>
2018-02-14 21:26:32 -0500

    add method

---
+++ add/src/com/israelmesa/Main.java
@@ -0,0 +1,15 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Write a function that returns the sum of two numbers.
+        int param1 = 1;
+        int param2 = 2;
+        System.out.println(add(param1, param2));
+
+    }
+    public static int add (int a, int b) {
+        return a + b;
+    }
+}
