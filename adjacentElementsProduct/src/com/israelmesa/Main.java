8546e7399d9789ca90e046038da8f04e3c24be19
Israel Mesa <mesa.israel50@gmail.com>
2018-02-16 23:47:20 -0500

    adjacentElementsProduct method

--- adjacentElementsProduct/src/com/israelmesa/Main.java
+++ adjacentElementsProduct/src/com/israelmesa/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
+        int[] numbers = {-23, 4, -3, 8, -12};
         adjacentElementsProduct(numbers);
 
     }
-    private static int[] adjacentElementsProduct(int[] inputArray) {
+    private static int adjacentElementsProduct(int[] inputArray) {
         int[] largeProducts = new int[inputArray.length - 1];
         int countDown = inputArray.length - 2;
         for (int i = 0; i < inputArray.length; i++) {
@@ -21,6 +21,13 @@ public class Main {
         for (int j = 0; j < largeProducts.length; j++) {
             System.out.println(largeProducts[j]);
         }
-        return largeProducts;
+        int largest = largeProducts[0];
+        for (int j = 0; j < largeProducts.length; j++) {
+            if (largest < largeProducts[j]){
+                largest = largeProducts[j];
+            }
+        }
+        System.out.println("Largest: " + largest);
+        return largest;
     }
 }
