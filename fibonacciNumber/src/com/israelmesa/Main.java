e6ed2c042463c8b620f990c6907967ec1fa0cbfb
Israel Mesa <mesa.israel50@gmail.com>
2018-02-24 12:51:34 -0500

    fibonacciNumber method

---
+++ fibonacciNumber/src/com/israelmesa/Main.java
@@ -0,0 +1,21 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int one = 7;
+        System.out.println(fibonacciNumber(one));
+
+//        int two = 7;
+//        fibonacciNumber(two);
+    }
+
+    public static int fibonacciNumber(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        } else {
+            System.out.println(n + ": " + (n - 1) + " + " + (n - 2) + " = " + ((n - 1) + (n - 2)));
+            return fibonacciNumber(n - 1) + fibonacciNumber(n - 2);
+        }
+    }
+}
