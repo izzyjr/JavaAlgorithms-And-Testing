9cf5454855ceb2a9c7a947ea9e9445381100419f
Israel Mesa <mesa.israel50@gmail.com>
2018-02-25 12:13:44 -0500

    matrixElementsSum method

--- matrixElementsSum/src/com/israelmesa/Main.java
+++ matrixElementsSum/src/com/israelmesa/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[][] matrix = {{1,1,1,0,1},
-                          {0,0,0,0,2},
+        int[][] matrix = {{1,0,1,0,1},
+                          {0,0,0,0,0},
                           {2,1,3,10,0},
                           {2,1,3,10,2},
                           {2,1,3,10,2}};
@@ -19,9 +19,9 @@ public class Main {
 
         for (int row = 0; row < matrix.length; row++) {
             for (int column = 0; column < matrix[row].length; column++) {
-                if (row != matrix.length - 1) {
+                if (row < matrix.length - 1) {
                     if (matrix[row][column] == 0) {
-                        for (int i = row + 1; i <= matrix[row].length - 1; i++) {
+                        for (int i = row; i <= matrix[row].length - 1; i++) {
                             matrix[i][column] = 0;
                         }
                     }
