3ac2df4039b7fe5cd7e316d325199a60dad7d5ff
Israel Mesa <mesa.israel50@gmail.com>
2018-07-23 20:47:00 -0400

    Count of Elements with Max Value

--- allLongestStrings/src/com/israelmesa/Main.java
+++ allLongestStrings/src/com/israelmesa/Main.java
@@ -4,7 +4,54 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //algorithm time again
+        String[] arrayOne = {"aba", "aa", "ad", "vcd", "aba"};
 
+        allLongestStrings(arrayOne);
+
+    }
+
+    private static String[] allLongestStrings(String[] inputArray) {
+
+        int max = 0;
+        int count = 0;
+        String[] allLongestStrings;
+
+        for (int i = 0; i < inputArray.length; i++) {
+
+            if (inputArray[i].length() > max) {
+
+                max  = inputArray[i].length();
+
+            }
+
+        }
+
+        for (int j = 0; j < inputArray.length; j++) {
+
+            if (inputArray[j].length() ==  max) {
+
+                count++;
+
+            }
+
+        }
+
+        allLongestStrings = new String[count];
+
+        for (int k = 0; k < inputArray.length; k++) {
+
+            if (inputArray[k].length() ==  max) {
+
+                allLongestStrings[k] = inputArray[k];
+
+            }
+
+        }
+
+        System.out.println("Max: " + max);
+        System.out.println("Count: " + count);
+
+
+        return allLongestStrings;
     }
 }
