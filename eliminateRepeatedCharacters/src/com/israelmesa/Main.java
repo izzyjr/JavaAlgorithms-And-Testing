5621bdfa3758709722b6d5da3d0e8c35c4d4b0ae
Israel Mesa <mesa.israel50@gmail.com>
2018-07-26 20:21:40 -0400

    first commit

---
+++ eliminateRepeatedCharacters/src/com/israelmesa/Main.java
@@ -0,0 +1,14 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String string = "characters";
+    }
+
+    private static String eliminateRepeatedCharacters(String string) {
+
+        return string;
+
+    }
+}
