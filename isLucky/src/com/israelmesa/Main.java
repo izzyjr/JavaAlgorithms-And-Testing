de3f7ededcd5a379027568894860b72b03eef278
Israel Mesa <mesa.israel50@gmail.com>
2018-07-28 17:37:34 -0400

    first commit

---
+++ isLucky/src/com/israelmesa/Main.java
@@ -0,0 +1,25 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int n = 1230;
+        //returns true
+
+//        int n = 239017;
+        //return false
+
+        isLucky(n);
+
+    }
+
+    private static boolean isLucky(int n) {
+
+
+
+        return false;
+
+    }
+}
+
