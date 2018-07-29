a1102748742cdb1f93e62e5724461a602f9f2739
Israel Mesa <mesa.israel50@gmail.com>
2018-07-29 12:45:00 -0400

    first commit

---
+++ sortByHeight/src/com/israelmesa/Main.java
@@ -0,0 +1,24 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] testOne = {-1, 150, 190, 170, -1, -1, 160, 180};
+        //Output: [-1, 150, 160, 170, -1, -1, 180, 190]
+
+        int[] testTwo = {-1, -1, -1, -1, -1};
+        //Output: [-1, -1, -1, -1, -1]
+
+        int[] testThree = {4, 2, 9, 11, 2, 16};
+        //Output: [2, 2, 4, 9, 11, 16]
+
+        sortByHeight(testOne);
+
+    }
+
+    private static int[] sortByHeight(int[] a) {
+
+        return a;
+    }
+}
