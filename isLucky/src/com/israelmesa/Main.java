7ea3ba8d06570401f99260699247e470a5320bfc
Israel Mesa <mesa.israel50@gmail.com>
2018-07-28 21:40:20 -0400

    progress - 0

--- isLucky/src/com/israelmesa/Main.java
+++ isLucky/src/com/israelmesa/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int n = 1230;
+//        int n = 1230;
         //returns true
 
-//        int n = 239017;
+        int n = 239017;
         //return false
 
         isLucky(n);
@@ -16,10 +16,73 @@ public class Main {
 
     private static boolean isLucky(int n) {
 
+        int teamA = 0;
+        int teamB = 0;
+        String numberString = String.valueOf(n);
+        String numberArray[] = numberString.split("");
 
+        int splitLength = (numberString.length() / 2);
+
+        for (int i = 0; i < numberArray.length; i++) {
+
+            if (i < splitLength) {
+
+                teamA += Integer.parseInt(numberArray[i]);
+
+            } else {
+
+                teamB += Integer.parseInt(numberArray[i]);
+
+            }
+
+        }
+
+        if (teamA == teamB) {
+
+            return true;
+
+        }
 
         return false;
 
     }
+
+//    private static boolean isLucky(int n) {
+//
+//        int teamA = 0;
+//        int teamB = 0;
+//        String numberString = String.valueOf(n);
+//        String numberArray[] = numberString.split("");
+//
+//        int splitLength = (numberString.length() / 2);
+//
+//        for (int i = 0; i < numberArray.length; i++) {
+//
+//            if (i < splitLength) {
+//
+//                teamA += Integer.parseInt(numberArray[i]);
+//
+//            } else {
+//
+//                teamB += Integer.parseInt(numberArray[i]);
+//
+//            }
+//
+//        }
+//
+//        System.out.println("TeamA: " + teamA + " - " + "TeamB: " + teamB);
+//
+//        if (teamA == teamB) {
+//
+//            System.out.println("True");
+//            return true;
+//
+//        }
+//
+//        System.out.println("False");
+//        return false;
+//
+//    }
+
 }
 
