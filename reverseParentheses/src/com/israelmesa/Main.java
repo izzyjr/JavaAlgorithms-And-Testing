a6dfb97ed711768d40e8a3cd970d8c3b60da826b
Israel Mesa <mesa.israel50@gmail.com>
2018-08-08 16:22:20 -0400

    Progress - 3

--- reverseParentheses/src/com/israelmesa/Main.java
+++ reverseParentheses/src/com/israelmesa/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        String one = "a(bc)de";
+        String one = "abcde";
         //Output: "acbde"
 
         String two = "a(bcdefghijkl(mno)p)q";
@@ -24,69 +24,11 @@ public class Main {
 
         int openParCount = 0;
         int closeParCount = 0;
-        int openIndex;
-        int closeIndex = 0;
-        int increase = 0;
-        int decrease;
-        int countParen = 0;
+        int temp;
+
         String array[] = s.split("");
-//        List<String> list = new ArrayList<String>(Arrays.asList(s.split("")));
+        List<String> stringList = new ArrayList<String>(Arrays.asList(s.split("")));
         List<String> list = new ArrayList<String>();
-
-        for (int i = 0; i < array.length; i++) {
-
-            if (array[i].equals("(")) {
-
-                openParCount++;
-                openIndex = i + 1;
-
-                for (int j = i + 1; j < array.length; j++) {
-
-                    if (array[j].equals("(")) {
-
-                        openParCount++;
-
-                    } else if (array[j].equals(")")) {
-
-                        closeParCount++;
-
-                    }
-
-                    if (openParCount == closeParCount && openParCount > 0 && closeParCount > 0) {
-
-//                        System.out.println(openParCount +  " - " + closeParCount);
-
-                        closeIndex = j;
-                        decrease = j - 1;
-
-                        while (openIndex <= decrease) {
-
-                            list.add(array[decrease]);
-                            decrease--;
-                            openParCount = 0;
-                            closeParCount = 0;
-
-                        }
-
-                    }
-
-                }
-
-                i = closeIndex;
-
-            } else  {
-
-                list.add(array[i]);
-
-            }
-
-        }
-
-        for (int k = 0; k < list.size(); k++) {
-
-            System.out.println(list.get(k));
-
-        }
 
 
 
@@ -94,3 +36,142 @@ public class Main {
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    private static String reverseParentheses(String s) {
+//
+//        int openParCount = 0;
+//        int closeParCount = 0;
+//        int openIndex;
+//        int closeIndex = 0;
+//        int increase = 0;
+//        int decrease;
+//        int countParen = 0;
+//        String array[] = s.split("");
+////        List<String> list = new ArrayList<String>(Arrays.asList(s.split("")));
+//        List<String> list = new ArrayList<String>();
+//
+//        for (int i = 0; i < array.length; i++) {
+//
+//            if (array[i].equals("(")) {
+//
+//                openParCount++;
+//                openIndex = i + 1;
+//
+//                for (int j = i + 1; j < array.length; j++) {
+//
+//                    if (array[j].equals("(")) {
+//
+//                        openParCount++;
+//
+//                    } else if (array[j].equals(")")) {
+//
+//                        closeParCount++;
+//
+//                    }
+//
+//                    if (openParCount == closeParCount && openParCount > 0 && closeParCount > 0) {
+//
+////                        System.out.println(openParCount +  " - " + closeParCount);
+//
+//                        closeIndex = j;
+//                        decrease = j - 1;
+//
+//                        while (openIndex <= decrease) {
+//
+//                            list.add(array[decrease]);
+//                            decrease--;
+//                            openParCount = 0;
+//                            closeParCount = 0;
+//
+//                        }
+//
+//                    }
+//
+//                }
+//
+//                i = closeIndex;
+//
+//            } else  {
+//
+//                list.add(array[i]);
+//
+//            }
+//
+//        }
+//
+//        for (int k = 0; k < list.size(); k++) {
+//
+//            System.out.println(list.get(k));
+//
+//        }
+//
+//
+//
+//        return s;
+//
+//    }
+
+//
+//        for (int i =  0; i < stringList.size(); i++) {
+//
+//        if (stringList.get(i).equals("(")) {
+//
+//        openParCount++;
+//
+//        } else if (stringList.get(i).equals(")")) {
+//
+//        closeParCount++;
+//
+//        }
+//
+//        }
