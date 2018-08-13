68cadead6381e6f2bc043f8b29d6204ee3474467
Israel Mesa <mesa.israel50@gmail.com>
2018-08-13 19:19:44 -0400

    first commit

---
+++ alternatingSums/src/com/israelmesa/Main.java
@@ -0,0 +1,26 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] one = {50, 60, 60, 45, 70};
+        //Output [180, 105]
+
+        alternatingSums(one);
+
+
+    }
+
+    private static int[] alternatingSums(int[] a) {
+
+        int[] array = new int[2];
+
+
+        return array;
+
+    }
+
+
+}
