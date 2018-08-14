89a3fcef4a185a9ecb9c67608e703da76e112db4
Israel Mesa <mesa.israel50@gmail.com>
2018-08-14 09:38:05 -0400

    first commit

---
+++ addBorder/src/com/israelmesa/Main.java
@@ -0,0 +1,31 @@
+package com.israelmesa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] pictureOne = {"abc", "ded"};
+        //Output: ["*****","*abc*","*ded*","*****"]
+
+        String[] pictureTwo = {"a"};
+        //Output: "***", "*a*", "***"
+
+        String[] pictureThree = {"aa", "**", "zz"};
+        //Output: "****","*aa*","****","*zz*","****"
+
+        String[] pictureFour = {"wzy**"};
+        //Output: "*******", "*wzy***","*******"
+
+
+
+    }
+
+    private static String[] addBorder(String[] picture) {
+
+
+
+
+        return picture;
+
+    }
+}
