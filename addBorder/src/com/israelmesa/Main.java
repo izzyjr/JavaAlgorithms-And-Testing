0dcbf1a170b0e754a046bc3aa77cf67f94a9aca3
Israel Mesa <mesa.israel50@gmail.com>
2019-01-17 19:42:44 -0500

    first commit

--- addBorder/src/com/israelmesa/Main.java
+++ addBorder/src/com/israelmesa/Main.java
@@ -16,16 +16,41 @@ public class Main {
         String[] pictureFour = {"wzy**"};
         //Output: "*******", "*wzy***","*******"
 
-
+        addBorder(pictureOne);
 
     }
 
     private static String[] addBorder(String[] picture) {
 
+        int newArraySize = picture.length + 2;
+        String[] framedPicture = new String[newArraySize];
+        int stringLength = picture[0].length() + 2;
+        int orginalArrayLengthCounter = 0;
 
+        for (int i = 0; i < framedPicture.length; i++) {
 
+            if (i == 0 || i == framedPicture.length - 1) {
+                String topBottom = "*****";
+                framedPicture[i] = topBottom;
+            } else {
+                framedPicture[i] = picture[orginalArrayLengthCounter];
+                orginalArrayLengthCounter++;
+            }
 
-        return picture;
+        }
+
+        printPicture(framedPicture);
+
+        return framedPicture;
 
     }
+
+    private static void printPicture(String[] framedPicture) {
+
+        for (int i = 0; i < framedPicture.length; i++) {
+            System.out.println("\n" + framedPicture[i]);
+        }
+
+    }
+
 }
