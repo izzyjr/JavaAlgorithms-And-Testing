71df61dbddf38762fe6d0ccd6711ee792d80baa4
Israel Mesa <mesa.israel50@gmail.com>
2019-01-20 23:13:54 -0500

    Execution time limit exceeded

--- areSimilar/src/com/israelmesa/Main.java
+++ areSimilar/src/com/israelmesa/Main.java
@@ -1,23 +1,62 @@
 package com.israelmesa;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        // test 1
-        int[] oneA = {1, 2, 3};
-        int[] oneB = {1, 2, 3};
+        // test 1 - false
+        int[] oneA = {1, 2, 2};
+        int[] oneB = {2, 1, 1};
 
-        sameNumbers(oneA, oneB);
+        // test 2 - false
+        int[] twoA = {4, 6, 3};
+        int[] twoB = {3, 4, 6,};
 
+        // test 3 - true
+        int[] threeA = {1, 2, 3};
+        int[] threeB = {1, 2, 3};
 
+        // test 4 - true
+        int[] fourA = {1, 2, 3};
+        int[] fourB = {2, 1, 3};
 
+        areSimilar(fourA, fourB);
 
     }
 
     public static boolean areSimilar(int[] a, int[] b) {
 
+        int arrayLength = a.length;
+        int counter = 0;
+        int[] realA = Arrays.copyOf(a, a.length);
+        int[] realB = Arrays.copyOf(b, b.length);
+
+        if (sameNumbers(a, b)) {
+
+            for (int i = 0; i < arrayLength; i++) {
+
+                if (realA[i] != realB[i]) {
+
+                    counter++;
+
+                }
+
+            }
+
+            if (counter > 2) {
+
+                return false;
+
+            }
+
+            return true;
+
+        }
+
         return false;
+
     }
 
     public static boolean sameNumbers(int[] a, int[] b) {
