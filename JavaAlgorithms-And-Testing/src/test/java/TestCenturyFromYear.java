92f365505ef7f8c8b6010e145ad5c50a2dc6acbc
Israel Mesa <mesa.israel50@gmail.com>
2022-04-29 19:23:01 -0400

    data provider rename

--- JavaAlgorithms-And-Testing/src/test/java/TestCenturyFromYear.java
+++ JavaAlgorithms-And-Testing/src/test/java/TestCenturyFromYear.java
@@ -5,12 +5,12 @@ import static org.testng.Assert.assertEquals;
 
 public class TestCenturyFromYear {
 
-    @Test(dataProvider = "ValuesToAdd")
+    @Test(dataProvider = "yearAndCentury")
     private void testCenturyFromYear(int year, int expectedResult) {
         assertEquals(CenturyFromYear.centuryFromYear(year), expectedResult);
     }
 
-    @DataProvider(name="ValuesToAdd")
+    @DataProvider(name="yearAndCentury")
     private Object[][] getData() {
         return new Object[][]{{2001, 21}, {1988, 20}, {1700, 17}, {1499, 15}};
     }
