461fb014899bc1cf47addae02d53639426835ed9
Israel Mesa <mesa.israel50@gmail.com>
2022-04-30 22:04:01 -0400

    adding older version

--- JavaAlgorithms-And-Testing/src/main/java/MakeArrayConsecutive2.java
+++ JavaAlgorithms-And-Testing/src/main/java/MakeArrayConsecutive2.java
@@ -25,4 +25,36 @@ public class MakeArrayConsecutive2 {
             array[i] = newElement;
         }
     }
+
+    // first attempt from way back
+    public static int makeArrayConsecutiveTwo0(int[] statues) {
+        if (statues.length == 1) {
+            return 0;
+        }
+        int largestNum = statues[0];
+        int smallestNum = statues[0];
+        for (int i = 0; i < statues.length; i++) {
+            if (largestNum <= statues[i]) {
+                largestNum = statues[i];
+            }
+            if (smallestNum >= statues[i]) {
+                smallestNum = statues[i];
+            }
+        }
+
+        int countUp = smallestNum;
+        int there = 0;
+        for (int j = 0; j < largestNum; j++) {
+            countUp++;
+            for (int k = 0; k < statues.length; k++) {
+                if (countUp < largestNum) {
+                    if (countUp == statues[k]) {
+                        there++;
+                    }
+                }
+            }
+        }
+        int count = (largestNum - (smallestNum + 1)) - there;
+        return count;
+    }
 }
