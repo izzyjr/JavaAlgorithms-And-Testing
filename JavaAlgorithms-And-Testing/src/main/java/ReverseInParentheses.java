18bdd8af973e4c8a9ca5ef9b61eb9a4af8159ba8
Israel Mesa <mesa.israel50@gmail.com>
2022-05-08 21:25:25 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/ReverseInParentheses.java
+++ JavaAlgorithms-And-Testing/src/main/java/ReverseInParentheses.java
@@ -1,35 +1,72 @@
-import com.sun.source.tree.WhileLoopTree;
+import java.util.Arrays;
 
 public class ReverseInParentheses {
 
     public static String reverseInParentheses(String string) {
 
-        StringBuilder stringBuilder = new StringBuilder(string);
+        char[] array = string.toCharArray();
+        int next = 0;
 
-        for (int i = 0; i < stringBuilder.length(); i++) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("where we at: " + i);
             int openBracketCount = 0;
             int closedBracketCount = 0;
-            if (stringBuilder.charAt(i) == '(') {
+            if (array[i] == '(') {
                 openBracketCount++;
                 int start = i;
                 int end = i;
                 int iterator = i;
-                while (end == start) {
-                    ++iterator;
-                    if (stringBuilder.charAt(iterator) == '(') {
+                while (end == start && ++iterator < string.length()) {
+                    if (array[iterator] == '(') {
                         openBracketCount++;
-                    } else if (stringBuilder.charAt(iterator) == ')') {
+                        if (openBracketCount == 2) {
+                            next = iterator;
+                        }
+                        System.out.println("next - " + next);
+                    } else if (array[iterator] == ')') {
                         closedBracketCount++;
                         if (openBracketCount == closedBracketCount) {
                             end = iterator;
                         }
                     }
                 }
-                System.out.println(start + " " + end);
+                System.out.println(start + " - " + end);
+                int k = start;
+                for (int j = end; j >= start; j--) {
+                    if (string.charAt(j) != '(' && string.charAt(j) != ')') {
+                        if (array[k] == '(' || array[k] == ')') {
+                            int count = k;
+                            while (array[count] == '(' || array[count] == ')') {
+                                ++count;
+                                array[count] = string.charAt(j);
+                            }
+                            k = count;
+                            k++;
+                        } else {
+                            array[k] = string.charAt(j);
+                            k++;
+                        }
+                    }
+                }
+                if (openBracketCount == 2) {
+                    i = next - 1;
+                }
             }
         }
-
-        return stringBuilder.toString();
+        return Arrays.toString(array);
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
