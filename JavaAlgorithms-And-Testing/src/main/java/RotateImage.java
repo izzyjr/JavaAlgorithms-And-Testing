c9b88446da3428f63d639d74443d6c5783b8ba98
Israel Mesa <mesa.israel50@gmail.com>
2022-05-11 19:49:15 -0400

    rotateImage

---
+++ JavaAlgorithms-And-Testing/src/main/java/RotateImage.java
@@ -0,0 +1,21 @@
+import com.sun.source.tree.WhileLoopTree;
+
+public class RotateImage {
+
+    public static int[][] rotateImage(int[][] a) {
+
+        int colsLength = a[0].length;
+        int rowsLength = a.length;
+        int[][] result = new int[colsLength][rowsLength];
+        int j = 0;
+
+        for (int rows = rowsLength - 1; rows >= 0; rows--) {
+            for (int cols = 0; cols < colsLength; cols++) {
+                result[cols][j] = a[rows][cols];
+            }
+            j++;
+        }
+        return result;
+    }
+}
+
---
+++ JavaAlgorithms-And-Testing/src/test/java/TestRotateImage.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestRotateImage {
+
+    @DataProvider(name="matrix")
+    private Object[][] getData() {
+        return new Object[][]{new int[][] {{1, 2, 3},
+                                           {4, 5, 6},
+                                           {7, 8, 9}}
+        };
+    }
+
+    @Test(dataProvider = "matrix")
+    private void testRotateImage(int[][] a) {
+
+        int[][] expectedResult = new int[][] {{7, 4, 1},
+                {8, 5, 2},
+                {9, 6, 3}};
+
+        assertEquals(RotateImage.rotateImage(a), expectedResult);
+    }
+}
