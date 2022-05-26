aa23ad76ee906974cff3baa1b7bbca715f0a2320
Israel Mesa <mesa.israel50@gmail.com>
2022-05-26 19:00:27 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/GroupingDishes.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/GroupingDishes.java
@@ -1,9 +1,40 @@
 package hashtables;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class GroupingDishes {
 
     public static String[][] groupingDishes(String[][] dishes) {
 
+        int rowsLength = dishes.length;
+        int firstItemInRow = 0;
+        String dish = null;
+
+        Map<String, List<String>> map = new LinkedHashMap<String, List<String>>();
+        for (int rows = 0; rows < rowsLength; rows++) {
+            for (int cols = 0; cols < dishes[rows].length; cols++) {
+                if (rows == firstItemInRow && cols == 0) {
+                    dish = dishes[rows][cols];
+                } else {
+                    map.put(dishes[rows][cols], new ArrayList<>(List.of(dish)));
+                }
+            }
+            firstItemInRow++;
+        }
+
+        printHashtable(map);
+
         return dishes;
+    }
+
+    public static void printHashtable(Map<String, List<String>> map) {
+        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+            for (String s : entry.getValue()) {
+                System.out.println(entry.getKey() + " - " + s);
+            }
+        }
     }
 }
