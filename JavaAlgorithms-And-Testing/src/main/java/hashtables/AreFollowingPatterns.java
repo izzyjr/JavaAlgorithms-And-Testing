f656460edbeb97623e400eec78cb9392791333b6
Israel Mesa <mesa.israel50@gmail.com>
2022-05-30 09:41:14 -0400

    areFollowingPatterns

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/AreFollowingPatterns.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/AreFollowingPatterns.java
@@ -2,6 +2,7 @@ package hashtables;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class AreFollowingPatterns {
 
@@ -10,9 +11,9 @@ public class AreFollowingPatterns {
         Map<String, String> hashMap = new HashMap<>();
 
         for (int i = 0; i < strings.length; i++) {
-            if (!hashMap.containsKey(patterns[i])) {
+            if (!hashMap.containsKey(patterns[i]) && !hashMap.containsValue(strings[i])) {
                 hashMap.put(patterns[i], strings[i]);
-            } else if (hashMap.get(patterns[i]) != strings[i]) {
+            } else if (!Objects.equals(hashMap.get(patterns[i]), strings[i])) {
                 return false;
             }
         }
