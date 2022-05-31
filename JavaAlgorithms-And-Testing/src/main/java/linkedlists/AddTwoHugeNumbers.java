df5aedf0ad28afb935d13c7e1777c0bafd064810
Israel Mesa <mesa.israel50@gmail.com>
2022-05-31 16:49:53 -0400

    in progress 1.2

--- JavaAlgorithms-And-Testing/src/main/java/linkedlists/AddTwoHugeNumbers.java
+++ JavaAlgorithms-And-Testing/src/main/java/linkedlists/AddTwoHugeNumbers.java
@@ -24,23 +24,25 @@ public class AddTwoHugeNumbers {
         ListNode<Integer> head = new ListNode<>(0);
         ListNode<Integer> pointer = head;
 
-        int count = 1;
+        int count = 0;
         String fourDigits = "";
 
         for (int i = 0; i < stringNum.length(); i++) {
-            if (count <= 4) {
+            if (count < 4) {
                 if (fourDigits == "" && stringNum.charAt(i) == '0') {
                     i++;
+                    count++;
                 } else {
                     fourDigits += stringNum.charAt(i);
+                    count++;
                 }
-                count++;
-            } else {
-                pointer.value = Integer.parseInt(fourDigits);
-                fourDigits = "";
-                pointer.next = new ListNode<>(0);
-                pointer = pointer.next;
-                count = 1;
+                if (count == 4) {
+                    pointer.value = Integer.parseInt(fourDigits);
+                    fourDigits = "";
+                    pointer.next = new ListNode<>(0);
+                    pointer = pointer.next;
+                    count = 0;
+                }
             }
         }
 
