fe4d4bdc4d545a6fd58566041c8708e99d0e2784
Israel Mesa <mesa.israel50@gmail.com>
2022-06-01 12:06:59 -0400

    first attempt solution - addBorder

--- JavaAlgorithms-And-Testing/src/main/java/AddBorder.java
+++ JavaAlgorithms-And-Testing/src/main/java/AddBorder.java
@@ -13,4 +13,43 @@ public class AddBorder {
 
         return framed;
     }
+
+    // first attempt solution
+    public static String[] addBorder0(String[] picture) {
+
+        int newArraySize = picture.length + 2;
+        String[] framedPicture = new String[newArraySize];
+        int stringLength = picture[0].length() + 2;
+        int orginalArrayLengthCounter = 0;
+        String topBottom = topBottomFrameGenerator(stringLength);
+
+        for (int i = 0; i < framedPicture.length; i++) {
+            if (i == 0 || i == framedPicture.length - 1) {
+                framedPicture[i] = topBottom;
+            } else {
+                framedPicture[i] = "*" + picture[orginalArrayLengthCounter] + "*";
+                orginalArrayLengthCounter++;
+            }
+        }
+
+        printPicture(framedPicture);
+        return framedPicture;
+    }
+
+    private static void printPicture(String[] framedPicture) {
+
+        for (int i = 0; i < framedPicture.length; i++) {
+            System.out.println(framedPicture[i]);
+        }
+    }
+
+    private static String topBottomFrameGenerator(int length) {
+
+        String topBottomFrame = "";
+        for (int i = 0; i < length; i++) {
+            topBottomFrame = topBottomFrame.concat("*");
+        }
+
+        return topBottomFrame;
+    }
 }
