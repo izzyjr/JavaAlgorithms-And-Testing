4d5392d10f7f9e44fa61168589cba6ac6db041e3
Israel Mesa <mesa.israel50@gmail.com>
2022-06-01 12:16:14 -0400

    first attempt solution - sortByHeight

--- JavaAlgorithms-And-Testing/src/main/java/SortByHeight.java
+++ JavaAlgorithms-And-Testing/src/main/java/SortByHeight.java
@@ -17,4 +17,37 @@ public class SortByHeight {
         }
         return array;
     }
+
+    // first attempt solution
+    public static int[] sortByHeight0(int[] a) {
+
+        int temp;
+        int increase = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                if (j < (a.length - 1)) {
+                    if (a[j + 1] == -1) {
+                        while (a[j + increase] == -1 && (j + increase) < (a.length - 1)) {
+                            increase++;
+                        }
+                        if (a[j] > a[j + increase] && a[j + increase] != -1) {
+                            temp = a[j + increase];
+                            a[j + increase] = a[j];
+                            a[j] = temp;
+                        }
+                    } else  {
+                        if (a[j] > a[j + 1]) {
+                            temp = a[j + 1];
+                            a[j + 1] = a[j];
+                            a[j] = temp;
+                        }
+                    }
+                }
+                increase = 1;
+            }
+        }
+
+        return a;
+    }
 }
