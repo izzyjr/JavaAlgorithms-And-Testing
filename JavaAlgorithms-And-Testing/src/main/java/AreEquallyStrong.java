7b7febe58f8a01069c526970b31a5d10bdb612cf
Israel Mesa <mesa.israel50@gmail.com>
2022-06-05 16:52:54 -0400

    areEquallyStrong - improved

--- JavaAlgorithms-And-Testing/src/main/java/AreEquallyStrong.java
+++ JavaAlgorithms-And-Testing/src/main/java/AreEquallyStrong.java
@@ -2,28 +2,7 @@ public class AreEquallyStrong {
 
     public static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
 
-        int myStrongest = 0;
-        int myWeakest = 0;
-        int friendsStrongest = 0;
-        int friendsWeakest = 0;
-
-        if (yourLeft >= yourRight) {
-            myStrongest = yourLeft;
-            myWeakest = yourRight;
-        } else {
-            myStrongest = yourRight;
-            myWeakest = yourLeft;
-        }
-
-        if (friendsLeft >= friendsRight) {
-            friendsStrongest = friendsLeft;
-            friendsWeakest = friendsRight;
-        } else {
-            friendsStrongest = friendsRight;
-            friendsWeakest = friendsLeft;
-        }
-
-        return myStrongest == friendsStrongest && myWeakest == friendsWeakest;
+        return yourLeft == friendsLeft && yourRight == friendsRight || yourLeft == friendsRight && yourRight == friendsLeft;
     }
 
     // first attempt solution
