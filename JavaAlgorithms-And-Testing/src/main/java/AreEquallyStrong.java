c2ec51b24cbea2b59c628e8516ff178774470a02
Israel Mesa <mesa.israel50@gmail.com>
2022-06-05 16:47:53 -0400

    first attempt solution - areEquallyStrong

--- JavaAlgorithms-And-Testing/src/main/java/AreEquallyStrong.java
+++ JavaAlgorithms-And-Testing/src/main/java/AreEquallyStrong.java
@@ -25,4 +25,22 @@ public class AreEquallyStrong {
 
         return myStrongest == friendsStrongest && myWeakest == friendsWeakest;
     }
+
+    // first attempt solution
+    public static boolean areEquallyStrong0(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+
+        int[] me = {yourLeft, yourRight};
+        int[] friend = {friendsLeft, friendsRight};
+
+        if ((yourLeft + yourRight) == (friendsLeft + friendsRight)) {
+            for (int i = 0; i < me.length; i++) {
+                for (int j = 0; j < friend.length; j++) {
+                    if (me[i] == friend[j]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 }
