9b2eea30142687714deef525ba031b8bfbe2ae94
Israel Mesa <mesa.israel50@gmail.com>
2022-06-22 22:35:43 -0400

    in progress 1.0

---
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/PossibleSums.java
@@ -0,0 +1,9 @@
+package hashtables;
+
+public class PossibleSums {
+
+    public static int possibleSums(int[] coins, int[] quantity) {
+
+        return 0;
+    }
+}
---
+++ JavaAlgorithms-And-Testing/src/test/java/hashtables/TestPossibleSums.java
@@ -0,0 +1,21 @@
+package hashtables;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestPossibleSums {
+
+    @DataProvider(name="inputOutput")
+    private Object[][] getData() {
+        return new Object[][]{
+                {new int[] {10, 50, 100}, new int[] {10, 50, 100}, 9},
+        };
+    }
+
+    @Test(dataProvider = "inputOutput")
+    private void testPossibleSums(int[] coins, int[] quantity, int expectedResult) {
+        assertEquals(PossibleSums.possibleSums(coins, quantity), expectedResult);
+    }
+}
