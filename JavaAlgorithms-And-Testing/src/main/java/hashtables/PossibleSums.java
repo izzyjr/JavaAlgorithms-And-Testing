5632a13009d6f8c46c4b986d0a2cbb8f1ce7dc5e
Israel Mesa <mesa.israel50@gmail.com>
2022-06-24 22:41:19 -0400

    in progress 1.0

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/PossibleSums.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/PossibleSums.java
@@ -1,18 +1,8 @@
 package hashtables;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class PossibleSums {
 
     public static int possibleSums(int[] coins, int[] quantity) {
-
-        Map<Integer, Integer> coinQuantity = new HashMap<>();
-        int length = coins.length;
-
-        for (int i = 0; i < length; i++) {
-            coinQuantity.put(coins[i], quantity[i]);
-        }
 
         return 0;
     }
