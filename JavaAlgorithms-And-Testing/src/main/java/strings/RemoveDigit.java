eddced240604715e8bd5ec97e9a600f992365351
Israel Mesa <mesa.israel50@gmail.com>
2022-06-27 21:19:20 -0400

    removeDigit

--- JavaAlgorithms-And-Testing/src/main/java/strings/RemoveDigit.java
+++ JavaAlgorithms-And-Testing/src/main/java/strings/RemoveDigit.java
@@ -5,18 +5,18 @@ public class RemoveDigit {
     public static String removeDigit(String number, char digit) {
 
         int length = number.length();
-        long max = 0;
+        String max = "0";
 
         for (int i = 0; i < length; i++) {
             StringBuilder editable = new StringBuilder(number);
             if (number.charAt(i) == digit) {
-                long value = Long.parseLong(String.valueOf(editable.deleteCharAt(i)));
-                if (value > max) {
+                String value = String.valueOf(editable.deleteCharAt(i));
+                if (value.compareTo(max) > 0) {
                     max = value;
                 }
             }
         }
 
-        return String.valueOf(max);
+        return max;
     }
 }
