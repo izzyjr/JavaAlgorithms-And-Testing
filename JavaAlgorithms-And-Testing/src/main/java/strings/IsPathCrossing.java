95c2a71fd319c63e965f9246abb5e27aef30da4d
Israel Mesa <mesa.israel50@gmail.com>
2022-06-30 21:44:29 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/strings/IsPathCrossing.java
+++ JavaAlgorithms-And-Testing/src/main/java/strings/IsPathCrossing.java
@@ -1,8 +1,29 @@
 package strings;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class IsPathCrossing {
 
     public static boolean isPathCrossing(String path) {
+
+        Map<Integer, ArrayList<Integer>> coordinates = new HashMap<>();
+        coordinates.put(0, new ArrayList<>(List.of(0)));
+        int[] currently = {0, 0};
+
+        for (int i = 0; i < path.length(); i++) {
+            if (path.charAt(i) == 'N') {
+                
+            } else if (path.charAt(i) == 'S') {
+
+            } else if (path.charAt(i) == 'W') {
+
+            } else if (path.charAt(i) == 'E') {
+
+            }
+        }
 
         return false;
     }
