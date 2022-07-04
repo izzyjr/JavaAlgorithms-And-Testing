64e5c90939ecb6447b5ca4cfd905803b63a66c06
Israel Mesa <mesa.israel50@gmail.com>
2022-07-04 19:18:42 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/CanConstruct.java
+++ JavaAlgorithms-And-Testing/src/main/java/CanConstruct.java
@@ -2,6 +2,19 @@ public class CanConstruct {
 
     public static boolean canConstruct(String ransomNote, String magazine) {
 
-        return false;
+        int count = 0;
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            for (int j = 0; j < magazine.length(); j++) {
+                if (ransomNote.charAt(i) == magazine.charAt(j)) {
+                    count++;
+                    if (i < ransomNote.length() - 1) {
+                        i++;
+                    }
+                }
+            }
+        }
+
+        return count == ransomNote.length();
     }
 }
