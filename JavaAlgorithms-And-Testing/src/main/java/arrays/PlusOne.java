ea052b00f9290a18319f5ef30194843c3c950b0f
Israel Mesa <mesa.israel50@gmail.com>
2022-07-06 13:16:32 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/arrays/PlusOne.java
+++ JavaAlgorithms-And-Testing/src/main/java/arrays/PlusOne.java
@@ -1,10 +1,23 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class PlusOne {
 
     public static int[] plusOne(int[] digits) {
 
-        digits[digits.length - 1] += 1;
-        return digits;
+        String strValue = Arrays.toString(digits).replaceAll("[,\\[\\]\\s]", "");
+        int strValueLength = strValue.length();
+        long longValue = Long.parseLong(strValue) + 1;
+        String strValuePlusOne = Long.toString(longValue);
+        int valuePlusOneLength = strValuePlusOne.length();
+
+        int[] digitsModified = new int[valuePlusOneLength];
+
+        for (int i = 0; i < valuePlusOneLength; i++) {
+            digitsModified[i] = Integer.parseInt(String.valueOf(strValuePlusOne.charAt(i)));
+        }
+
+        return digitsModified;
     }
 }
--- JavaAlgorithms-And-Testing/src/test/java/arrays/TestPlusOne.java
+++ JavaAlgorithms-And-Testing/src/test/java/arrays/TestPlusOne.java
@@ -11,6 +11,9 @@ public class TestPlusOne {
     private Object[][] getData() {
         return new Object[][]{
                 {new int[] {1, 2, 3}, new int[] {1, 2, 4}},
+                {new int[] {9}, new int[] {1, 0}},
+                {new int[] {9, 9, 9}, new int[] {1, 0, 0, 0}},
+                {new int[] {8, 9, 9, 9}, new int[] {9, 0, 0, 0}}
         };
     }
 
