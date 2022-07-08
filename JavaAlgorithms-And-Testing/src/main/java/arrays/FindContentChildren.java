782c5957fc50e811f8b4cbc835efc4a2f687f4e4
Israel Mesa <mesa.israel50@gmail.com>
2022-07-08 12:40:59 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/arrays/FindContentChildren.java
+++ JavaAlgorithms-And-Testing/src/main/java/arrays/FindContentChildren.java
@@ -5,13 +5,26 @@ public class FindContentChildren {
     public static int findContentChildren(int[] g, int[] s) {
 
         int count = 0;
+        int min = Integer.MAX_VALUE;
+        int diff = 0;
+        int index = 0;
 
         for (int i = 0; i < g.length; i++) {
             for (int j = 0; j < s.length; j++) {
-                if (g[i] >= s[j]) {
+                if (g[i] == s[j]) {
                     count++;
                     s[j] = -1;
                     break;
+                } else if (g[i] < s[j]) {
+                    diff = s[j] - g[i];
+                    if (diff < min) {
+                        min = diff;
+                        index = j;
+                    }
+                }
+                if (j == s.length - 1) {
+                    s[index] = -1;
+                    count++;
                 }
             }
         }
