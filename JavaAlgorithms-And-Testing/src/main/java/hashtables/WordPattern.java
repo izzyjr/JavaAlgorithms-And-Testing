b9067075a0c59da23066c86dc3c34a58a923d401
Israel Mesa <mesa.israel50@gmail.com>
2022-07-15 18:11:49 -0400

    in progress 1.0

---
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/WordPattern.java
@@ -0,0 +1,9 @@
+package hashtables;
+
+public class WordPattern {
+
+    public static boolean wordPattern(String pattern, String s) {
+
+        return false;
+    }
+}
---
+++ JavaAlgorithms-And-Testing/src/test/java/hashtables/TestWordPattern.java
@@ -0,0 +1,21 @@
+package hashtables;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestWordPattern {
+
+    @DataProvider(name="inputOutput")
+    private Object[][] getData() {
+        return new Object[][]{
+                {"abba", "dog cat cat dog", true}
+        };
+    }
+
+    @Test(dataProvider = "inputOutput")
+    private void testWordPattern(String pattern, String s, boolean expectedResult) {
+        assertEquals(WordPattern.wordPattern(pattern, s), expectedResult);
+    }
+}
