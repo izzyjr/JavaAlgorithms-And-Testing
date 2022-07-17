e2f63bd46668ce871b844653126a4776021248fd
Israel Mesa <mesa.israel50@gmail.com>
2022-07-16 23:39:03 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/WordPattern.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/WordPattern.java
@@ -1,8 +1,24 @@
 package hashtables;
 
+import java.util.ArrayList;
+
 public class WordPattern {
 
     public static boolean wordPattern(String pattern, String s) {
+
+        ArrayList<String> words = new ArrayList<>();
+        String word = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
+                word += s.charAt(i);
+            } else {
+                words.add(word);
+                word = "";
+            }
+        }
+
+        words.add(word);
 
         return false;
     }
