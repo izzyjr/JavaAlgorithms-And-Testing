355b9314cb0b25285d6c86bd9ddc1f45c7c3e480
Israel Mesa <mesa.israel50@gmail.com>
2022-08-03 14:05:45 -0400

    in progress 1.0

---
+++ JavaAlgorithms-And-Testing/src/main/java/strings/ValidAnagram.java
@@ -0,0 +1,9 @@
+package strings;
+
+public class ValidAnagram {
+
+    public static Boolean validAnagram(String s, String t) {
+
+        return true;
+    }
+}
---
+++ JavaAlgorithms-And-Testing/src/test/java/strings/TestValidAnagram.java
@@ -0,0 +1,21 @@
+package strings;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestValidAnagram {
+
+    @DataProvider(name="stringsAndResult")
+    private Object[][] getData() {
+        return new Object[][] {
+                {"anagram", "nagaram", true}
+        };
+    }
+
+    @Test(dataProvider="stringsAndResult")
+    private void testValidAnagram(String s, String t, Boolean expectedResult) {
+        assertEquals(ValidAnagram.validAnagram(s, t), expectedResult);
+    }
+}
