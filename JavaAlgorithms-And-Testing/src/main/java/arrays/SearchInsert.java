f787105858b2dd7f84c5dafd3e4974804fa39f5e
Israel Mesa <mesa.israel50@gmail.com>
2022-08-15 16:58:15 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/arrays/SearchInsert.java
+++ JavaAlgorithms-And-Testing/src/main/java/arrays/SearchInsert.java
@@ -5,11 +5,19 @@ public class SearchInsert {
     public static int searchInsert(int[] nums, int target) {
 
         int result = 0;
+        int length = nums.length;
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < length; i++) {
             if (nums[i] == target) {
                 result = i;
                 break;
+            } else if (i < length - 2) {
+                if (nums[i] < target && nums[i + 2] > target) {
+                    result = i + 1;
+                    break;
+                }
+            } else if (i == length - 1) {
+                result = length;
             }
         }
 
--- JavaAlgorithms-And-Testing/src/test/java/arrays/TestSearchInsert.java
+++ JavaAlgorithms-And-Testing/src/test/java/arrays/TestSearchInsert.java
@@ -10,7 +10,9 @@ public class TestSearchInsert {
     @DataProvider(name="inputOutput")
     private Object[][] getData() {
         return new Object[][]{
-                {new int[] {1, 3, 5, 6}, 5, 2}
+                {new int[] {1, 3, 5, 6}, 5, 2},
+                {new int[] {1, 3, 5, 6}, 2, 1},
+                {new int[] {1, 3, 5, 6}, 7, 4}
         };
     }
 
