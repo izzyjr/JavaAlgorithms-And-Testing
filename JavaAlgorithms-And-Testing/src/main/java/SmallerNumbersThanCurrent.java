516253d0b21b1a9bcb2712acf2b80ead2ab2a037
Israel Mesa <mesa.israel50@gmail.com>
2022-08-24 23:50:44 -0400

    smallerNumbersThanCurrent

--- JavaAlgorithms-And-Testing/src/main/java/SmallerNumbersThanCurrent.java
+++ JavaAlgorithms-And-Testing/src/main/java/SmallerNumbersThanCurrent.java
@@ -2,7 +2,18 @@ public class SmallerNumbersThanCurrent {
 
     public static int[] smallerNumbersThanCurrent(int[] nums) {
 
-        int[] result = new int[nums.length];
+        int length = nums.length;
+        int[] result = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            int count = 0;
+            for (int j = 0; j < length; j++) {
+                if (nums[i] > nums[j]) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
 
         return result;
     }
--- JavaAlgorithms-And-Testing/src/test/java/TestSmallerNumbersThanCurrent.java
+++ JavaAlgorithms-And-Testing/src/test/java/TestSmallerNumbersThanCurrent.java
@@ -8,7 +8,9 @@ public class TestSmallerNumbersThanCurrent {
     @DataProvider(name="inputOutput")
     private Object[][] getData() {
         return new Object[][]{
-                {new int[] {8, 1, 2, 2, 3}, new int[] {14, 0, 1, 1, 3}}
+                {new int[] {8, 1, 2, 2, 3}, new int[] {4, 0, 1, 1, 3}},
+                {new int[] {6, 5, 4, 8}, new int[] {2, 1, 0, 3}},
+                {new int[] {7, 7, 7, 7}, new int[] {0, 0, 0, 0}}
         };
     }
 
