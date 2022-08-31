008092446271d91ea68e39b2bf6c6a9aaf9b1b98
Israel Mesa <mesa.israel50@gmail.com>
2022-08-30 22:56:40 -0400

    in progress 1.6

--- JavaAlgorithms-And-Testing/src/main/java/strings/BuddyStrings.java
+++ JavaAlgorithms-And-Testing/src/main/java/strings/BuddyStrings.java
@@ -1,28 +1,25 @@
 package strings;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class BuddyStrings {
 
     public static boolean buddyStrings(String s, String goal) {
 
         int length = s.length();
-        StringBuilder string = new StringBuilder(s);
-        Map<Integer, Character> map = new HashMap<>();
+        int count = 0;
 
         for (int i = 0; i < length; i++) {
             if (s.charAt(i) != goal.charAt(i)) {
-                map.put(i, goal.charAt(i));
+                count++;
+            }
+            if (i != 0) {
+                if (s.charAt(i - 1) == s.charAt(i)) {
+                    count++;
+                }
             }
         }
 
-        System.out.println(map.size());
+        System.out.println(count);
 
-        for (Map.Entry<Integer, Character> entry : map.entrySet()) {
-            string.setCharAt(entry.getKey(), entry.getValue());
-        }
-
-        return string.toString().equals(goal);
+        return count > 0 && count <= 2;
     }
 }
