f75d22695e19ea842493706a5bedb3dfddc75f0c
Israel Mesa <mesa.israel50@gmail.com>
2022-09-08 22:52:21 -0400

    in progress 1.0

---
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/DistributeCandies.java
@@ -0,0 +1,12 @@
+package hashtables;
+
+public class DistributeCandies {
+
+    public static int distributeCandies(int[] candyType) {
+
+        int result = 0;
+
+
+        return result;
+    }
+}
---
+++ JavaAlgorithms-And-Testing/src/test/java/hashtables/TestDistributeCandies.java
@@ -0,0 +1,21 @@
+package hashtables;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestDistributeCandies {
+
+    @DataProvider(name="inputOutput")
+    private Object[][] getData() {
+        return new Object[][]{
+                {new int[] {1, 1, 2, 2, 3, 3}, 3}
+        };
+    }
+
+    @Test(dataProvider = "inputOutput")
+    private void testDistributeCandies(int[] nums, int expectedResult) {
+        assertEquals(DistributeCandies.distributeCandies(nums), expectedResult);
+    }
+}
