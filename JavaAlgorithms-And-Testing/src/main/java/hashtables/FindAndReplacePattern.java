7ac7787b29ae975de6a576e7725a444623a0a68b
Israel Mesa <mesa.israel50@gmail.com>
2022-09-13 21:36:03 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/FindAndReplacePattern.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/FindAndReplacePattern.java
@@ -1,10 +1,32 @@
 package hashtables;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindAndReplacePattern {
 
     public static String[] findAndReplacePattern(String[] words, String pattern) {
 
         String[] result = new String[10];
+        String keys = "";
+
+        for (String word : words) {
+
+            Map<Character, Character> map = new HashMap<>();
+            boolean flag = true;
+
+            for (int j = 0; j < word.length(); j++) {
+                if (!map.containsKey(pattern.charAt(j)) && !map.containsValue(word.charAt(j))) {
+                    map.put(pattern.charAt(j), word.charAt(j));
+                } else if (map.get(pattern.charAt(j)) != word.charAt(j)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+
+            }
+        }
 
         return result;
     }
