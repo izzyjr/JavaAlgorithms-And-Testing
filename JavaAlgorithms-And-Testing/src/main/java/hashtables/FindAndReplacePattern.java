ae0b4ed37b636ac002a74bfdc3f5dfa40271c3c3
Israel Mesa <mesa.israel50@gmail.com>
2022-09-14 21:42:54 -0400

    in progress 1.2

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/FindAndReplacePattern.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/FindAndReplacePattern.java
@@ -2,30 +2,38 @@ package hashtables;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FindAndReplacePattern {
 
     public static String[] findAndReplacePattern(String[] words, String pattern) {
 
-        String[] result = new String[10];
-        String keys = "";
+        StringBuilder keys = new StringBuilder();
+        int length = words.length;
 
-        for (String word : words) {
+        for (int i = 0; i < length; i++) {
 
             Map<Character, Character> map = new HashMap<>();
             boolean flag = true;
 
-            for (int j = 0; j < word.length(); j++) {
-                if (!map.containsKey(pattern.charAt(j)) && !map.containsValue(word.charAt(j))) {
-                    map.put(pattern.charAt(j), word.charAt(j));
-                } else if (map.get(pattern.charAt(j)) != word.charAt(j)) {
+            for (int j = 0; j < pattern.length(); j++) {
+                if (!map.containsKey(pattern.charAt(j)) && !map.containsValue(words[i].charAt(j))) {
+                    map.put(pattern.charAt(j), words[i].charAt(j));
+                } else if (!Objects.equals(map.get(pattern.charAt(j)), words[i].charAt(j))) {
                     flag = false;
                     break;
                 }
             }
             if (flag) {
-
+                keys.append(i);
             }
+        }
+
+        int keysLength = keys.length();
+        String[] result = new String[keysLength];
+
+        for (int k = 0; k < keysLength; k++) {
+            result[k] = words[Integer.parseInt(String.valueOf(keys.charAt(k)))];
         }
 
         return result;
