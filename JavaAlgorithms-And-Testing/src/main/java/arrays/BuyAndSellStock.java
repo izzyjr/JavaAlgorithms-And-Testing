4689541fe26dd781f8456d82dfce8c0787b023b3
Israel Mesa <mesa.israel50@gmail.com>
2023-05-12 14:28:14 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/arrays/BuyAndSellStock.java
+++ JavaAlgorithms-And-Testing/src/main/java/arrays/BuyAndSellStock.java
@@ -6,10 +6,15 @@ public class BuyAndSellStock {
 
         int maxProfit = 0;
 
-        for (int i = 0; i < prices.length; i++) {
+        for (int i = 0; i < prices.length - 1; i++) {
             for (int j = i + 1; j < prices.length; j++) {
                 if (maxProfit < prices[j] - prices[i]) {
                     maxProfit = prices[j] - prices[i];
+                }
+            }
+            if (i + 1 < prices.length) {
+                if (prices[i] < prices[i + 1]){
+                    i++;
                 }
             }
         }
