59570e5afb2562c4ab504f30abe61065f91e0c3a
Israel Mesa <mesa.israel50@gmail.com>
2023-05-29 15:21:49 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/strings/ValidParentheses.java
+++ JavaAlgorithms-And-Testing/src/main/java/strings/ValidParentheses.java
@@ -8,22 +8,40 @@ public class ValidParentheses {
             return false;
         }
 
-        StringBuilder secondHalf = new StringBuilder();
+        String secondHalf = "";
+        int half = s.length() / 2;
 
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' && s.charAt(i + 1) != ')') {
-                secondHalf.append(')');
-            }
-            if (s.charAt(i) == '[' && s.charAt(i + 1) != ']') {
-                secondHalf.append(')');
-            }
-            if (s.charAt(i) == '{' && s.charAt(i + 1) != '}') {
-                secondHalf.append(')');
+            if (i + 1 < s.length()) {
+                if (i == half - 1) {
+                    if (s.charAt(i) == '(') {
+                        secondHalf += ')';
+                    } else if (s.charAt(i) == '[') {
+                        secondHalf += ']';
+                    } else if (s.charAt(i) == '{') {
+                        secondHalf += '}';
+                    }
+                } else {
+                    if (s.charAt(i) == '(' && s.charAt(i + 1) != ')') {
+                        secondHalf += ')';
+                    } else if (s.charAt(i) == '[' && s.charAt(i + 1) != ']') {
+                        secondHalf += ']';
+                    } else if (s.charAt(i) == '{' && s.charAt(i + 1) != '}') {
+                        secondHalf += '}';
+                    }
+                }
             }
         }
 
-        if (secondHalf.length() != 0) {
+        System.out.println("second half: " + secondHalf);
 
+        if (secondHalf.length() != 0) {
+            for (int j = secondHalf.length() - 1; j > 0; j--) {
+                if (secondHalf.charAt(j) != s.charAt(half)) {
+                    return false;
+                }
+                half++;
+            }
         }
 
         return true;
--- JavaAlgorithms-And-Testing/src/test/java/strings/TestValidParentheses.java
+++ JavaAlgorithms-And-Testing/src/test/java/strings/TestValidParentheses.java
@@ -11,7 +11,7 @@ public class TestValidParentheses {
     private Object[][] getData() {
         return new Object[][] {
                 {"()[]{}", true},
-                {"())", true},
+                {"(())", true},
                 {"{]", false},
                 {"(({[]}))", true}
         };
