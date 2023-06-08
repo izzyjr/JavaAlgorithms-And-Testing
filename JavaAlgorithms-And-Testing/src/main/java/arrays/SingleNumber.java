5683f882b16f90f5f49fee4b288cb9fbe5730ba5
Israel Mesa <mesa.israel50@gmail.com>
2023-06-08 09:48:52 -0400

    singleNumber - improved

--- JavaAlgorithms-And-Testing/src/main/java/arrays/SingleNumber.java
+++ JavaAlgorithms-And-Testing/src/main/java/arrays/SingleNumber.java
@@ -9,18 +9,16 @@ public class SingleNumber {
     public static int singleNumber(int[] nums) {
 
         Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-        map.put(1, new ArrayList<>());
         map.put(0, new ArrayList<>());
 
         for (int num : nums) {
-            if (!map.get(1).contains(num) && !map.get(0).contains(num)) {
-                map.get(1).add(num);
-            } else if (map.get(1).contains(num)) {
-                map.get(1).remove((Integer) num);
+            if (map.get(0).contains(num)) {
+                map.get(0).remove((Integer) num);
+            } else {
                 map.get(0).add(num);
             }
         }
 
-        return map.get(1).get(0);
+        return map.get(0).get(0);
     }
 }
