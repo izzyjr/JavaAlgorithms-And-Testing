3f5b01ec3cc565daea0d5cad75e2c9ada4ff1e57
Israel Mesa <mesa.israel50@gmail.com>
2023-06-28 13:54:06 -0400

    in progress 1.2

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
@@ -7,17 +7,26 @@ public class LongestHarmoniousSubsequence {
         int longest = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            int count = 1;
-            boolean minMax = false;
+            int max = 0;
+            int min = 0;
+            int count = 0;
             for (int j = i + 1; j < nums.length; j++) {
-                if (Math.abs(nums[i] - nums[j]) == 1) {
-                    count++;
-                    minMax = true;
-                } else if (nums[i] == nums[j]) {
+                if (nums[i] - nums[j] == 1) {
+                    max = nums[i];
+                    min = nums[j];
+                    break;
+                } else if (nums[i] - nums[j] == -1) {
+                    max = nums[j];
+                    min = nums[i];
+                    break;
+                }
+            }
+            for (int x = i; x < nums.length; x++) {
+                if (nums[x] == max || nums[x] == min) {
                     count++;
                 }
             }
-            if (longest < count && minMax) {
+            if (longest < count) {
                 longest = count;
             }
         }
