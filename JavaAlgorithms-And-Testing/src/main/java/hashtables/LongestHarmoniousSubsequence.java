c9f74d9e258d24982d04c1be9393e4a09d284479
Israel Mesa <mesa.israel50@gmail.com>
2023-06-29 15:37:35 -0400

    in progress 1.3

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
@@ -1,33 +1,36 @@
 package hashtables;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class LongestHarmoniousSubsequence {
 
     public static int longestHarmoniousSubsequence(int[] nums) {
 
-        int longest = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> keys = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int max = 0;
-            int min = 0;
-            int count = 0;
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] - nums[j] == 1) {
-                    max = nums[i];
-                    min = nums[j];
-                    break;
-                } else if (nums[i] - nums[j] == -1) {
-                    max = nums[j];
-                    min = nums[i];
-                    break;
-                }
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+                keys.add(num);
             }
-            for (int x = i; x < nums.length; x++) {
-                if (nums[x] == max || nums[x] == min) {
-                    count++;
+        }
+
+        int longest = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            for (Integer key : keys) {
+                int sum = 0;
+                if (entry.getKey() - key == 1) {
+                    sum = entry.getValue() + map.get(key);
+                    if (longest < sum) {
+                        longest = sum;
+                    }
                 }
-            }
-            if (longest < count) {
-                longest = count;
             }
         }
 
