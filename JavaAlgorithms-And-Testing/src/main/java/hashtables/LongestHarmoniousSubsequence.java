5750b86c62596c430cfd56882601534b6c6a7554
Israel Mesa <mesa.israel50@gmail.com>
2023-06-30 13:13:00 -0400

    longestHarmoniousSubsequence - refactored

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/LongestHarmoniousSubsequence.java
@@ -1,29 +1,23 @@
 package hashtables;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LongestHarmoniousSubsequence {
 
     public static int longestHarmoniousSubsequence(int[] nums) {
 
         Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> keys = new ArrayList<>();
 
         for (int num : nums) {
-            if (map.containsKey(num)) {
-                map.put(num, map.get(num) + 1);
-            } else {
-                map.put(num, 1);
-                keys.add(num);
-            }
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
 
-        Collections.sort(keys);
         int longest = 0;
 
-        for (int i = 0; i + 1 < keys.size(); i++) {
-            if (keys.get(i + 1) - keys.get(i) == 1) {
-                int length = map.get(keys.get(i + 1)) + map.get(keys.get(i));
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                int length = map.get(key) + map.get(key + 1);
                 if (longest < length) {
                     longest = length;
                 }
