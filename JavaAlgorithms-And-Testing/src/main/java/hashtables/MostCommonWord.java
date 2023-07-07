2bb2296dcb93c4c115aded4ce68dc8155a0b2f0b
Israel Mesa <mesa.israel50@gmail.com>
2023-07-07 13:44:00 -0400

    in progress 1.0

---
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/MostCommonWord.java
@@ -0,0 +1,35 @@
+package hashtables;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MostCommonWord {
+
+    public static String mostCommonWord(String paragraph, String[] banned) {
+
+        String[] words = paragraph.split("[\\s,.]+");
+        Map<String, Integer> wordCount = new HashMap<>();
+
+        for (String word : words) {
+            wordCount.put(word.toLowerCase(), wordCount.getOrDefault(word.toLowerCase(), 0) + 1);
+        }
+
+        for (String ban : banned) {
+            wordCount.remove(ban.toLowerCase());
+        }
+
+        System.out.println(wordCount);
+
+        String result = "";
+        int count = 0;
+
+        for (Map.Entry<String, Integer> entry : wordCount.entrySet()) {
+            if (count < entry.getValue()) {
+                count = entry.getValue();
+                result = entry.getKey();
+            }
+        }
+
+        return result;
+    }
+}
---
+++ JavaAlgorithms-And-Testing/src/test/java/hashtables/TestMostCommonWord.java
@@ -0,0 +1,22 @@
+package hashtables;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class TestMostCommonWord {
+
+    @DataProvider(name="inputOutput")
+    private Object[][] getData() {
+        return new Object[][]{
+                {"Bob hit a ball, the hit BALL flew far after it was hit.", new String[] {"hit"}, "ball"},
+                {"a.", new String[] {}, "a"}
+        };
+    }
+
+    @Test(dataProvider = "inputOutput")
+    private void testMostCommonWord(String paragraph, String[] banned, String expectedResult) {
+        assertEquals(MostCommonWord.mostCommonWord(paragraph, banned), expectedResult);
+    }
+}
