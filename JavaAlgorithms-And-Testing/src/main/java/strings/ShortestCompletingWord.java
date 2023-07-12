26790c900e9e380de73efcf136519e1aec44be74
Israel Mesa <mesa.israel50@gmail.com>
2023-07-12 13:06:02 -0400

    in progress 1.1

--- JavaAlgorithms-And-Testing/src/main/java/strings/ShortestCompletingWord.java
+++ JavaAlgorithms-And-Testing/src/main/java/strings/ShortestCompletingWord.java
@@ -1,11 +1,36 @@
 package strings;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ShortestCompletingWord {
 
     public static String shortestCompletingWord(String licensePlate, String[] words) {
 
-        String result = "";
+        String[] letters = licensePlate.replaceAll("[^a-zA-Z]", "").toLowerCase().split("");
+        Map<Character, Integer> wordMap = new HashMap<>();
+        int wordLength = Integer.MAX_VALUE;
+        String answer = "";
 
-        return result;
+        for (String word : words) {
+            for (int i = 0; i < words.length; i++) {
+                wordMap.put(word.charAt(i), wordMap.getOrDefault(word.charAt(i), 0) + 1);
+            }
+            int count = 0;
+            for (String s : letters) {
+                char letter = s.charAt(0);
+                if (wordMap.containsKey(letter)) {
+                    count++;
+                }
+            }
+            if (count == letters.length) {
+                if (wordLength > word.length()) {
+                    wordLength = word.length();
+                    answer = word;
+                }
+            }
+        }
+
+        return answer;
     }
 }
