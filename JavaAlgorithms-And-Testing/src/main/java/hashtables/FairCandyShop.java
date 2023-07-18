a4a1590d6c92c834e3da91b139e7d7ff7c1e4b08
Israel Mesa <mesa.israel50@gmail.com>
2023-07-18 14:06:19 -0400

    in progress 1.3

--- JavaAlgorithms-And-Testing/src/main/java/hashtables/FairCandyShop.java
+++ JavaAlgorithms-And-Testing/src/main/java/hashtables/FairCandyShop.java
@@ -1,39 +1,38 @@
 package hashtables;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class FairCandyShop {
 
     public static int[] fairCandySwap(int[] aliceSizes, int[] bobSizes) {
 
-        Map<Integer, Integer> aliceMap = new HashMap<>();
-        Map<Integer, Integer> bobMap = new HashMap<>();
         int aliceCandies = 0;
         int bobCandies = 0;
 
         for (int aliceSize : aliceSizes) {
             aliceCandies += aliceSize;
-            aliceMap.put(aliceSize, aliceMap.getOrDefault(aliceSize, 0) + 1);
         }
 
         for (int bobSize : bobSizes) {
             bobCandies += bobSize;
-            bobMap.put(bobSize, bobMap.getOrDefault(bobSize, 0) + 1);
         }
 
         int fairAmount = (aliceCandies + bobCandies) / 2;
-
-        System.out.println(aliceCandies);
-        System.out.println(aliceMap);
-        System.out.println(bobCandies);
-        System.out.println(bobMap);
-        System.out.println(fairAmount);
-
-
         int[] answer = new int[2];
 
+        for (int i = 0; i < aliceSizes.length; i++) {
+            for (int j = i + 1; j < bobSizes.length; j++) {
+                int aliceTotal = (aliceCandies + bobSizes[j]) - aliceSizes[i];
+                int bobTotal = (bobCandies + aliceSizes[i]) - bobSizes[j];
+                if (aliceTotal == fairAmount && bobTotal == fairAmount) {
+                    answer[0] = aliceSizes[i];
+                    answer[1] = bobSizes[j];
+                    break;
+                }
+            }
+        }
 
+        System.out.println(Arrays.toString(answer));
 
         return answer;
     }
