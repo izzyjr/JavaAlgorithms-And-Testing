0516cc8e28e3b3e0d34278467dc9da387d6e1686
Israel Mesa <mesa.israel50@gmail.com>
2025-08-20 23:31:49 -0400

    nit - correct import update

--- JavaAlgorithms-And-Testing/src/test/java/TestGetEvenDigitSum.java
+++ JavaAlgorithms-And-Testing/src/test/java/TestGetEvenDigitSum.java
@@ -1,7 +1,7 @@
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.Assert.assertEquals;
 
 public class TestGetEvenDigitSum {
 
